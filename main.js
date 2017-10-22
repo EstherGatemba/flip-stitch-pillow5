4a95fb10698bd17fa66021e132335b2321a7bc65
Esther <egatemba@andrew.cmu.edu>
2017-10-21 20:48:55 -0400

    Choice change to Productdetail

--- main.js
+++ main.js
@@ -4,22 +4,10 @@ function bear-long(name) {
   this.price=price;
   this.art=art;
   this.size=size;
-
 }
-
-
-/*Global variables*/
-var pillows = [new bear(), new Dog(), new bunny()];
-var names = ["bear-long", "bear", "bear2-square", "bunny", "bunny2-square", "bunny-round", "bunny-long", "dog", "dog2-square", "dog"];
-var colors = ["AquaWhite", "GreenBeige", "RedBlue"]
 
 /*Document load function*/
 $(document).ready(function(){
   $("#displayimage").attr("src","images/bear-long.jpg");
   console.log("page has loaded");
-
-  $(".offer").on("click", function () {
-      var $checkbox = $(this).find(':checkbox');
-      $checkbox.attr('checked', !$checkbox.attr('checked'));
-      console.log("checkbox checked")
 });
