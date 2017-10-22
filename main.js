be061f64f9b0dea267907f2b64a413ed11e373dd
Esther <egatemba@andrew.cmu.edu>
2017-10-21 22:16:18 -0400

    dropdown menu function

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 /*** Object Constructors ***/
-function bear-long(name) {
+function pillow() {
   this.name=name;
   this.price=price;
   this.art=art;
   this.size=size;
+  this.detail=detail;
 }
 
 /*Document load function*/
 $(document).ready(function(){
-  $("#displayimage").attr("src","images/bear-long.jpg");
-  console.log("page has loaded");
+  // $("#displayimage").attr("src","images/bear-long.jpg");
+  // console.log("page has loaded");
+
+$("size-dropdown").change (function(){
+  if (this.value == "long") {
+    $("image src").attr("src","images/bear-long.jpg")
+    $("textfield").text("Curl up with this long body pillow")
+
+  }else if (this.value) == "round") {
+    $("").attr("src","image/")
+    $("").text("yeah yeah yeah")
+
+  }else if (this.value) == "") {
+    $("").attr("src","image/")
+    $("").text("yeah yeah yeah")
+
+  }
+
+  }
+
 });
